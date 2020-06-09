eb1d4324ec83bfd6120b9272e9f5bb41892661c5
ticochuck <chucklivilla@gmail.com>
2020-06-08 17:26:04 -0700

    completed changes per Aliya, function is reusable with multimple files, added error log functionallity. Working on spliting the function into smaller functions

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,25 +1,7 @@
-requeted_words = [
-    'an Adjective', 
-    'an Adjective', 
-    'a First Name', 
-    'a Past Tense Verb', 
-    'a First Name',
-    'an Adjective', 
-    'an Adjective', 
-    'a Plural Noun', 
-    'a Large Animal', 
-    'an Small Animal', 
-    "a Girl's Name", 
-    'an Adjective', 
-    'a Plural Noun', 
-    'an Adjective', 
-    'a Plural Noun', 
-    'a Number 1-50', 
-    "a First Name's", 
-    'a Number', 
-    'a Plural Noun', 
-    'a Number', 
-    'a Plural Noun']
+
+from datetime import datetime
+
+dateTimeObj = datetime.now()
 words = []
 
 def welcome_mesage():
@@ -37,22 +19,40 @@ def welcome_mesage():
     to substitute for blanks in a story before 
     reading aloud. 
                             'Source: Wikipedia.'
+
     You will be prompted to enter a series of words/numbers.
     When you are done, the program will display a story
     based upon your input.
     ''')
 
-def words_input():
-    """
-    Function to promt the user to input the different words/numbers
-    to be used in the madlib template. 
-    """
-    for i in requeted_words:
-        
-        print(f'Please enter {i}')
-        word = input()
-        words.append(word)
-    print(f'words entered: {words}')
+def get_words(contents):
+    curly = "{"
+    last_word = False
+    count = 0
+    
+    while not(last_word):
+        for x in contents[:1]:
+            if curly not in contents:
+                last_word = True
+            else: 
+                a = contents.find('{')
+                b = contents.find('}') 
+                tobereplaced = contents[a:b+1]
+                print(f'Please enter {tobereplaced[1:-1]}')
+                word = input()
+                words.append(word)
+                value = words[count]
+                new_content = contents.replace(tobereplaced, value, 1)
+                contents = new_content
+            count += 1
+    
+    return new_content
+
+def write_new_content(new_template):
+    with open('./assets/final_text.txt', 'w') as final_text:
+            final_text.write(new_template)
+            print(new_template)
+
 
 def open_template():
     """
@@ -61,32 +61,27 @@ def open_template():
     try:
         with open('./assets/template.txt', 'r') as original_template:
             contents = original_template.read()
-            count = 0
-            while count < len(words):
-                for x in contents[:1]:
-                    a = contents.find('{')
-                    b = contents.find('}') 
-                    tobereplaced = contents[a:b+1]
-                    value = words[count]
-                    new_content = contents.replace(tobereplaced, value, 1)
-                    contents = new_content
-                count += 1
-
-        with open('./assets/final_text.txt', 'w') as final_text:
-            final_text.write(new_content)
-            print(new_content)
-    except FileNotFoundError:
-        with open('./assets/error_log.txt', 'w') as error_log:
-            error_log.write('File does not exits error')
+            
+            get_words(contents)
+            write_new_content(get_words)
+        
+    except FileNotFoundError as err:
+        with open('./assets/error_log.txt', 'a' ) as error_log:
+            error_log.write(f'\nFile does not exits error: \n{err}\n{str(dateTimeObj)}\n')
+    except TypeError as err:
+        with open('./assets/error_log.txt', 'a' ) as error_log:
+            error_log.write(f'\nType Error encountered: \n{err}\n{str(dateTimeObj)}\n')
 
 
 def main():
     """
     Calls the functions needed to run the program.
     """
-    welcome_mesage()
-    words_input()
-    open_template()
+    try:
+        welcome_mesage()
+        open_template()    
+    except KeyboardInterrupt:
+        print('Program was closed by the user')    
 
 main()
 
